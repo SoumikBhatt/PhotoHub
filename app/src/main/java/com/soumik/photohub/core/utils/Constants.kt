df56c228211d8e19eaf889fb336749826d44d443
Soumik Bhattacharjee <soumikcse07@gmail.com>
2022-06-15 23:13:26 +0600

    di injected

---
+++ app/src/main/java/com/soumik/photohub/core/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.soumik.photohub.core.utils
+
+object Constants {
+
+    const val BASE_URL = "https://api.unsplash.com/"
+    const val ACCESS_TOKEN = "AknAgONiTB1O1OkBAke5lOhBbIFjnyf6Ig_KFGNIcBs"
+    const val ERROR_MESSAGE = "Something went wrong! Please try again after sometime"
+    const val NO_ITEM_FOUND = "No item found!"
+    const val TIMEOUT_EXCEPTION_MESSAGE = "Internet Connection is too slow, please reconnect and try again"
+    const val NO_NETWORK_CONNECTION = "Internet connection not found, please check your internet connection."
+    const val INTERNAL_SERVER_ERROR = "Sorry! Server is down for now, please try again after sometime"
+    const val URL_ERROR = "Sorry! Something is wrong with the url, please try again after sometime"
+    const val FAVOURITE_INSERTION_FAILED = "Sorry! Can't insert favourite data, please try again later"
+
+    const val APP_EMAIL = "piyal.developer@gmail.com"
+
+    const val INITIAL_LOADING_ITEM_COUNT = 20
+}
---
+++ app/src/main/java/com/soumik/photohub/features/gallery/data/source/GalleryWebService.kt
@@ -0,0 +1,10 @@
+package com.soumik.photohub.features.gallery.data.source
+
+/**
+created by Soumik on 6/15/2022
+piyal.developer@gmail.com
+copyright (c) 2022 Soumik Bhattacharjee. All rights reserved
+ **/
+
+interface GalleryWebService {
+}
