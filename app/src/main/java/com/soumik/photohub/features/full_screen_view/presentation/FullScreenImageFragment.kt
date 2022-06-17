1b68acfc592db8a7818d773bb2f65aadd55319ec
Soumik Bhattacharjee <soumikcse07@gmail.com>
2022-06-17 21:50:04 +0600

    zoomable image view added

--- app/src/main/java/com/soumik/photohub/features/full_screen_view/presentation/FullScreenImageFragment.kt
+++ app/src/main/java/com/soumik/photohub/features/full_screen_view/presentation/FullScreenImageFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.soumik.photohub.core.utils.Constants
 import com.soumik.photohub.core.utils.showSnackBar
@@ -34,6 +35,11 @@ class FullScreenImageFragment : Fragment() {
         requireActivity().transparentStatusBar(true)
 
         setView()
+        viewListener()
+    }
+
+    private fun viewListener() {
+        mBinding.backBtn.setOnClickListener { findNavController().navigateUp() }
     }
 
     private fun setView() {
